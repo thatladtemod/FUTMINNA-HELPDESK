c86de6fcc81a7f7904168ac55badd4bdf9ac3c57
thatladtemod <temit3684@gmail.com>
2025-12-05 15:52:34 +0100

    chore: applied minimal design

--- function.js
+++ function.js
@@ -2,13 +2,26 @@ console.log("Hello there! This is thatladtemod's chatbot. How can I help you?")
 
 const userInput = document.getElementById("user-input");
 const sendBtn = document.getElementById("send-btn")
-const chat = document.querySelector(".chat-field")
+const chat = document.querySelector("#chat-field")
 let newmessage
 
 const user = () => {
     console.log(userInput.value);
     newmessage = document.createElement("div");
-    newmessage.textContent = userInput.value;
+    newmessage.innerHTML = `<div class="flex flex-row-reverse gap-3 w-2/3">
+            <div
+                class="h-8 w-8 rounded-full p-2 bg-purple-100 text-purple-600 flex justify-center items-center"
+                >
+                U
+            </div>
+            <div
+                id="user-message"
+                class="text-sm text-purple-600 leading-5 border-purple-100 shadow-md rounded-md p-2 font-semibold"
+                >
+                ${userInput.value}
+            </div>
+          </div>`;
+    newmessage.className = "flex w-full justify-end";
     chat.appendChild(newmessage);
     setTimeout(async () => {
         await assistant();
@@ -19,7 +32,10 @@ const user = () => {
 sendBtn.addEventListener("click", user)
 
 const assistant = async () => {
+
+    // store the value of newmessage in another variable 
     userText = newmessage
+
     try {
         if (userText) {
             const something = await fetch("replies.json");
@@ -30,7 +46,22 @@ const assistant = async () => {
 
             // Display message on screen
             let newbotmessage = document.createElement("div")
-            newbotmessage.textContent = response.message
+            newbotmessage.innerHTML = `
+            <div class="flex self-start gap-2 w-2/3">
+                <div
+                    class="h-8 w-8 rounded-full p-2 bg-purple-600 flex justify-center items-center"
+                >
+                    AI
+                </div>
+                <div
+                    id="assistant-message"
+                    class="text-sm text-gray-800 leading-5 border-purple-100 shadow-md rounded-md p-2 font-semibold"
+                >
+                ${response.message}
+                </div>
+           </div>
+           `;
+            newbotmessage.className = "flex w-full"
             chat.appendChild(newbotmessage)
         }
     } catch (error) {
